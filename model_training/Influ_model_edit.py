a33a0471ddb5e623ce51c9423308d8bb3c132149
vwfang <monkeyfangv@berkeley.edu>
2024-08-16 15:53:02 +0800

    Added example, model, model_training folders

---
+++ model_training/Influ_model_edit.py
@@ -0,0 +1,111 @@
+import os
+import tensorflow.keras
+from tensorflow.keras.layers import Dense, Dropout,Add
+from tensorflow.keras.layers import Embedding
+from tensorflow.keras.layers import Input,Flatten,Masking,BatchNormalization, Concatenate
+from tensorflow.keras.layers import LSTM,Conv1D,LeakyReLU, GlobalMaxPooling1D, MaxPooling1D
+from tensorflow.keras.regularizers import l2
+from tensorflow.keras import Model
+from tensorflow.keras import optimizers
+from tensorflow.keras.models import load_model
+
+
+def train_model(train_data, train_label, validation_data, validation_label, model_name, path = None):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    input_ = Input(shape=(50,6))
+    #mask = Masking(mask_value=0.)(input_)
+    #CNN Block 1
+    cnn1 = Conv1D(64 ,8,activation = 'relu', padding="same", kernel_regularizer=l2(0.01))(input_)
+    norm1 = BatchNormalization()(cnn1)
+    d1 = Dropout(0.6)(norm1)
+
+    #CNN Block2
+    cnn2 = Conv1D(32 ,8,activation = 'relu', padding="same", kernel_regularizer=l2(0.01))(d1)
+    norm2 = BatchNormalization()(cnn2)
+    #d2 = Dropout(0.5)(norm2)
+
+    #CNN Block 3
+    cnn3 = Conv1D(16 ,8,activation = 'relu', padding="same", kernel_regularizer=l2(0.01))(norm2)
+    norm3 = BatchNormalization()(cnn3)
+    #d3 = Dropout(0.5)(norm3)
+
+    #CNN Block 4
+    cnn4 = Conv1D(8 ,8,activation = 'relu', padding="same", kernel_regularizer=l2(0.01))(norm3)
+    norm4 = BatchNormalization()(cnn4)
+    d4 = Dropout(0.6)(norm4)
+    gmp4 = GlobalMaxPooling1D()(d4)
+
+    result = Dense(1, activation = "sigmoid")(gmp4)
+    model = Model(inputs=input_,outputs=result)
+    
+    model.load_weights('/home/vfang/home/AI4AVP_predictor_improved/model/model18_nn_PC_6_model_n_best_weights.h5', by_name=True)
+
+    for layer in model.layers[-3:]:
+        layer.trainable = False
+
+    model.compile(optimizer=optimizers.Adam(learning_rate=2e-4),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+
+
+    best_weights_filepath = path+'./%s_best_weights.h5'%model_name
+    saveBestModel = tensorflow.keras.callbacks.ModelCheckpoint(best_weights_filepath, 
+                                                        monitor='val_loss', 
+                                                        verbose=1, 
+                                                        save_best_only=True, 
+                                                        mode='auto')
+    reduce_lr = tensorflow.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                                              patience=50,verbose=1)
+    CSVLogger = tensorflow.keras.callbacks.CSVLogger(path+"./%s_csvLogger.csv"%model_name,separator=',', append=False)
+    e_s = tensorflow.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                          min_delta=0,
+                                          patience=150,
+                                          verbose=0, mode='min')
+
+    history = model.fit(train_data,train_label, validation_data=(validation_data, validation_label),shuffle=True,
+                    epochs=500, batch_size=10,callbacks=[saveBestModel,CSVLogger,reduce_lr,e_s])
+    return model,history
+
+
+
+
+
+def train_ennavia_model(train_data, train_label, validation_data, validation_label, model_name, path = None):
+    if not os.path.isdir(path):
+        os.mkdir(path)
+    input_ = Input(shape=(461))
+    #mask = Masking(mask_value=0.)(input_)
+    f1 = Dense(1024)(input_)
+    norm1 = BatchNormalization()(f1)
+    d1 = Dropout(0.3)(norm1)
+    f2 = Dense(256)(d1)
+    norm2 = BatchNormalization()(f2)
+    d2 = Dropout(0.3)(norm2)
+    f = Flatten()(d2)
+    result = Dense(1, activation = "sigmoid")(f)
+    model = Model(inputs=input_,outputs=result)
+
+    model.compile(optimizer=optimizers.Adam(lr=1e-4),
+                  loss='binary_crossentropy',
+                  metrics=['accuracy'])
+
+
+    best_weights_filepath = path+'./%s_best_weights.h5'%model_name
+    saveBestModel = tensorflow.keras.callbacks.ModelCheckpoint(best_weights_filepath, 
+                                                        monitor='val_loss', 
+                                                        verbose=1, 
+                                                        save_best_only=True, 
+                                                        mode='auto')
+    reduce_lr = tensorflow.keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.5,
+                                              patience=50,verbose=1)
+    CSVLogger = tensorflow.keras.callbacks.CSVLogger(path+"./%s_csvLogger.csv"%model_name,separator=',', append=False)
+    e_s = tensorflow.keras.callbacks.EarlyStopping(monitor='val_loss',
+                                          min_delta=0,
+                                          patience=150,
+                                          verbose=0, mode='min')
+
+    history = model.fit(train_data,train_label, validation_data=(validation_data, validation_label),shuffle=True,
+                    epochs=2000, batch_size=100,callbacks=[saveBestModel,CSVLogger,reduce_lr,e_s])
+    return model,history
---
+++ model_training/model_tools.py
@@ -0,0 +1,97 @@
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score,accuracy_score,f1_score,matthews_corrcoef,confusion_matrix,roc_curve,auc
+import matplotlib.pyplot as plt
+
+def split(data_encoded, labels, test_size=0.1, random_state = 10, save = True, output_root = None):
+    train_data, test_data, train_labels, test_labels = train_test_split(data_encoded, labels, test_size=0.1, random_state = 10, stratify = labels)
+    if save:
+        output_root = output_root or os.getcwd()
+        if not os.path.isdir(output_root):
+            os.mkdir(output_root)
+        json.dump(train_data.tolist(), open(os.path.join(output_root,'train_data.json'), "w"), indent=4) 
+        json.dump(test_data.tolist(), open(os.path.join(output_root,'test_data.json'), "w"), indent=4) 
+        json.dump(train_labels.tolist(), open(os.path.join(output_root,'train_labels.json'), "w"), indent=4) 
+        json.dump(test_labels.tolist(), open(os.path.join(output_root,'test_labels.json'), "w"), indent=4) 
+        print('train test encoded data and labels saved')
+    return  train_data, test_data, train_labels, test_labels
+
+# show train history
+#show_train_history(t_m ,'accuracy','val_accuracy')
+#show_train_history(t_m ,'loss','val_loss')
+def show_train_history(train_history,train,validation):
+    plt.plot(train_history.history[train])
+    plt.plot(train_history.history[validation])
+    plt.title('Train History')
+    plt.ylabel(train)
+    plt.xlabel('Epoch')
+    plt.legend(['train', 'validation'], loc='upper left')
+    plt.show()
+    
+# evalute metric (accuracy,precision,sensitivity,specificity,f1,mcc)
+def metric_array(test_data, test_labels, model):
+    labels_score = model.predict(test_data)
+    accuracy = accuracy_score(test_labels, labels_score.round())
+    confusion = confusion_matrix(test_labels, labels_score.round())
+    TP = confusion[1, 1]
+    TN = confusion[0, 0]
+    FP = confusion[0, 1]
+    FN = confusion[1, 0]
+    precision = TP / float(TP + FP)
+    sensitivity = TP / float(FN + TP)
+    specificity = TN / float(TN + FP)
+    f1 = f1_score(test_labels, labels_score.round())
+    mcc = matthews_corrcoef(test_labels, labels_score.round()) 
+    metric = [accuracy,precision,sensitivity,specificity,f1,mcc]
+    return {'accuracy':accuracy,
+           'precision':precision,
+           'sensitivity':sensitivity,
+           'specificity':specificity,
+           'f1':f1,
+           'mcc':mcc}
+
+def evalution_metrics(test_label, labels_score, save=False, txt_name=None, path = './'):
+    accuracy = accuracy_score(test_label, labels_score.round())
+    confusion = confusion_matrix(test_label, labels_score.round())
+    TP = confusion[1, 1]
+    TN = confusion[0, 0]
+    FP = confusion[0, 1]
+    FN = confusion[1, 0]
+    precision = TP / float(TP + FP)
+    sensitivity = TP / float(FN + TP)
+    specificity = TN / float(TN + FP)
+    f1 = f1_score(test_label, labels_score.round())
+    mcc = matthews_corrcoef(test_label, labels_score.round())
+    # precision TP / (TP + FP)
+    # recall: TP / (TP + FN)
+    # specificity : TN / (TN + FP)
+    # f1: 2 TP / (2 TP + FP + FN)
+    def evalution_metrics(test_label, labels_score, save=False, txt_name=None, path = './'):
+        accuracy = accuracy_score(test_label, labels_score.round())
+        confusion = confusion_matrix(test_label, labels_score.round())
+        TP = confusion[1, 1]
+        TN = confusion[0, 0]
+        FP = confusion[0, 1]
+        FN = confusion[1, 0]
+        precision = TP / float(TP + FP)
+        sensitivity = TP / float(FN + TP)
+        specificity = TN / float(TN + FP)
+        f1 = f1_score(test_label, labels_score.round())
+        mcc = matthews_corrcoef(test_label, labels_score.round())
+        # precision TP / (TP + FP)
+        # recall: TP / (TP + FN)
+        # specificity : TN / (TN + FP)
+        # f1: 2 TP / (2 TP + FP + FN)
+        metrics = [accuracy,precision,sensitivity,specificity,f1,mcc]
+        columns=['accuracy', 'precision', 'sensitivity', 'specificity', 'f1', 'mcc']
+        metrics_dict = dict(zip(columns,metrics))
+        if save:
+            df = pd.DataFrame(metrics_dict,index=[0])
+            df.to_csv(path+'%s_metrics.csv'%txt_name)
+            print('  # Accuracy: %f' % accuracy+'\n')
+            print('  # Precision: %f' % precision+'\n')  
+            print('  # Sensitivity/Recall: %f' % sensitivity+'\n')
+            print('  # Specificity: %f' %specificity+'\n')
+            print('  # F1 score: %f' % f1+'\n')
+            print('  # Matthews Corrcoef:%f' % mcc+'\n')
+        else:
+            return(metrics_dict)
